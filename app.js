a1eb6fc96fb8276f2320f2a026c7bb7f1815a2a0
POLETTI Stéphane <polettistephane@msn.com>
2023-02-28 16:19:45 +0100

    complete project /test ok /import-csv ok

--- app.js
+++ app.js
@@ -1,113 +1,66 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
 
-const { faker } = require("@faker-js/faker");
+const mysql = require("mysql2");
+const csv = require("csv-parser");
+const fs = require("fs");
+const results = [];
 
-//Model
-const fakerModel = require("./models/Utilisateur");
+//CREATE CONNECTION MYSQL DATABASE
+const db = mysql.createConnection({
+  host: "localhost",
+  port: "3000",
+  user: "root",
+  password: "root",
+  database: "vmitest",
+  socketPath: "/tmp/mysql.sock",
+});
 
-//View
-app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "views"));
+//CONNECT
+db.connect((err) => {
+  if (err) throw err;
+  console.log("mysql connected...");
+});
 
-// app.set("view engine", "ejs");
-// app.set("views", path.resolve(__dirname, "views"));
-
-// app.set("views", path.join(__dirname, "views"));
-
-// app.set("view engine", "ejs");
-
-// app.engine("ejs", require("ejs").__express);
-
-//VOIR VIDEO POIUR DETERMINER
-mongoose
-  .connect("mongodb://localhost:27017/BACK-END", { useNewUrlParser: true })
-  .then(() => console.log("connected to db"))
-  .catch((error) => console.log("connection error", error));
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
-// Initializing our variables with a different random data each time it is run
-// var randomName = faker.name.findName(); // Generates a random name
-// var randomEmail = faker.internet.email(); // Generates a random email
-// var randomProduct = faker.commerce.productName(); // Generates a random product name
-// var randomCompany = faker.company.companyName(); // Will give back a random company name
-// // var randomCard = faker.helpers.createCard(); // It's output is a random contact card containing many properties
-
-// // Iteration
-// // This code runs twenty times
-// // It produces each time different data
-// for (i = 0; i < 20; i++) {
-//   //   console.log(randomName); // Outputs a random name
-//   console.log(randomEmail); // Outputs a random email
-//   console.log(randomProduct); // Outputs the random product name generated
-//   console.log(randomCompany); // Produces a random company name
-//   //   console.log(randomCard); // Gives back a random card
-//   console.log(faker.date.past()); // Generates a random past date
-// }
-
-app.get("/", (req, res) => {
-  fakerModel.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else if (data) {
-      res.render("home", { data: data });
-    } else {
-      res.render("home", { data: {} });
-    }
+//CONVERT CSV IN ARRAY RESULTS
+fs.createReadStream("mockData.csv")
+  .pipe(csv({}))
+  .on("data", (data) => results.push(data))
+  .on("end", () => {
+    console.log(results);
   });
+
+console.log("console.log", results);
+
+//INSERT DONNEES CSV STOCKEES DANS RESULTS
+app.get("/import-csv", (req, res) => {
+  results.forEach((result) => {
+    let sql = `INSERT INTO user set ?`;
+    let query = db.query(sql, result, (err, result) => {
+      if (err) throw err;
+
+      console.log(result);
+    });
+  });
+
+  res
+    .status(200)
+    .json({ message: "import des données dans la base de données ..." });
 });
 
-app.post("/", (req, res, next) => {
-  for (let i = 0; i < 100000; i++) {
-    var fakee = new fakerModel({
-      uuid: faker.database.collation(),
-      nom: faker.name.lastName(),
-      email: faker.internet.email(),
-    });
-    fakee.save((err, data) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-  res.redirect("/");
+// GET DATAS
+// // Créer un serveur avec express et une methode get /test qui renvoit {”success”: true}
+app.get("/test", (req, res, next) => {
+  let sql = "select * from user";
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    // res.send("users fetched...");
+  });
+  res.status(200).json({ success: "true" });
 });
 
-// app.get("/test", (req, res, next) => {
-//   //   res.render("index", { title: "home" });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//   const utilisateurs = [
-//     {
-//       _id: "oeihfzeoi",
-//       uuid: "435535-444",
-//       nom: "POL",
-//       email: "pol@essai.fr",
-//     },
-//     {
-//       _id: "oeihf9887i",
-//       uuid: "435535-444",
-//       nom: "STE",
-//       email: "ste@essai.fr",
-//     },
-//   ];
-//   res.status(200).json(utilisateurs);
-// });
-
-var port = process.env.PORT || 3001;
-
-app.listen(port, () => console.log("server running at port " + port));
-
-module.exports = app;
+app.listen("3000"), () => console.log("server started");
---
+++ users.js
@@ -0,0 +1,6 @@
+// const users = [
+//   { id: 1, uuid: "23ER43", email: "pol@essai.com", nom: "POL" },
+//   { id: 2, uuid: "23ER44", email: "ste@essai.com", nom: "STE" },
+//   { id: 3, uuid: "23ER45", email: "rep@essai.com", nom: "REP" },
+// ];
+// module.exports = users;
